8e6f71209af1011ea97cb79f16d5c78ab6bae0d2
Ilham Setiabudi <59858608+ilhamsetiabudi@users.noreply.github.com>
2022-06-27 14:47:35 +0700

    Update db.js

--- src/helpers/databases/mongodb/db.js
+++ src/helpers/databases/mongodb/db.js
@@ -1,21 +1,21 @@
 const connection = require('./connection');
 
 const findAll = async (parameter, collection) => {
-    const db = connection.getDb();
+    const db = await connection.getDb();
     const recordset = await db.collection(collection).find(parameter).toArray();
     if (recordset.length < 1) return { err: true, data: recordset}
     return { err: null, data: recordset}
 };
 
 const findOne = async (parameter, collection) => {
-    const db = connection.getDb();
+    const db = await connection.getDb();
     const recordset = await db.collection(collection).findOne(parameter);
     if (!recordset) return { err: true, data: recordset}
     return { err: null, data: recordset}
 };
 
 const insertOne = async (document, collection) => {
-    const db = connection.getDb();
+    const db = await connection.getDb();
     const recordset = await db.collection(collection).insertOne(document);
     if (!recordset.acknowledged) return { err: true, data: null}
     return { err: null, data: document}
